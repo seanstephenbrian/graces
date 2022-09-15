b12c46b94dd9dd86d2249869313246370ab1f0a8
sean stephen brian <seanstephenbrian@gmail.com>
2022-09-15 17:16:31 -0400

    Design nav bar, get menu/close menu icons working

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function scrollUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+function toggleNav() {
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle("hide");
+}
