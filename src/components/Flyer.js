a19ea004a93741d5e74aafd922114564e93c8cf2
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-02-10 14:45:35 -0500

    work out logic for sorting/rendering events
    upcoming event flyers are rendered in chronological order, previous
    event flyers in reverse chronological order, and specific render
    conditions are laid out for when there are no upcoming / no previous
    events

--- src/components/Flyer.js
+++ src/components/Flyer.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/flyer.scss';
 
 export default function Flyer(props) {
 
+    // state:
+    const [eventStatus, setEventStatus] = useState();
+
     // props:
-    const { flyerFilename } = props;
+    const { 
+        flyerFilename,
+        upcoming } = props;
+
+    // hooks:
+    // determine event status:
+    useEffect(() => {
+        if (upcoming) {
+            setEventStatus('upcoming');
+        } else if (!upcoming) {
+            setEventStatus('previous');
+        }
+    }, [upcoming]);
 
     return (
-        <div className='event-flyer'>
-            <img 
+        <div className={`event-flyer ${eventStatus}-event-flyer`}>
+            <img
+                alt='event flyer' 
                 className='event-flyer-img'
                 src={require(`../img/flyers/${flyerFilename}`)} 
             />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Flyer from '../components/Flyer';
 
@@ -7,32 +7,83 @@ import '../styles/home.scss';
 export default function Home() {
 
     // state:
-    const [upcomingEvents, setUpcomingEvents] = useState([
+    const [events, setEvents] = useState([
         {
+            date: '2023-02-25',
             flyer: '2023-02-25.jpg'
         }
     ]);
 
     const [prevEvents, setPrevEvents] = useState([]);
+    const [upcomingEvents, setUpcomingEvents] = useState([]);
+
+    // hooks:
+    // generate previous/upcoming events arrays:
+    useEffect(() => {
+        let today = new Date();
+        let futureEvents = [];
+        let pastEvents = [];
+        events.forEach(event => {
+            const eventDate = new Date(event.date);
+            if (eventDate >= today) {
+                futureEvents.push(event);
+            } else if (eventDate < today) {
+                pastEvents.push(event);
+            }
+        });
+        // sort future events array in chronological order:
+        futureEvents.sort(function(a, b) {
+            return new Date(a.date) - new Date(b.date);
+        });
+        // sort past events array in reverse chronological order:
+        pastEvents.sort(function(a, b) {
+            return new Date(b.date) - new Date(a.date);
+        });
+        setPrevEvents(pastEvents);
+        setUpcomingEvents(futureEvents);
+    }, [events]);
 
     return (
         <div className='home-page'>
+
             <div className='upcoming-events-title'>
                 upcoming events:
             </div>
-            {upcomingEvents.map((event) => {
-                return (
-                    <Flyer flyerFilename={event.flyer} />
-                )
-            })}
-            <div className='previous-events-title'>
-                previous events:
-            </div>
-            {prevEvents.map((event) => {
-                return (
-                    <Flyer flyerFilename={event.flyer} />
-                )
-            })}
+
+            {upcomingEvents.length === 0 ?
+                // text to display if there are no upcoming events:
+                <div className='no-upcoming-events'>
+                    nothing scheduled right now, but check back soon!
+                </div>
+                // otherwise render a Flyer component for each event:
+                : upcomingEvents.map((event) => {
+                    return (
+                        <Flyer 
+                            flyerFilename={event.flyer}
+                            upcoming={true}
+                        />
+                    )
+                })
+            }
+           
+           {/* only render the previous events section if there are actually previous events: */}
+           {prevEvents.length > 0 ? 
+                <>
+                    <div className='previous-events-title'>
+                        previous events:
+                    </div>
+                    {prevEvents.map((event) => {
+                        return (
+                            <Flyer 
+                                flyerFilename={event.flyer}
+                                upcoming={false} 
+                            />
+                        )
+                    })}
+                </>
+                : '' 
+            }
+    
         </div>
     )
 }
