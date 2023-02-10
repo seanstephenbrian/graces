467a6de0284b8e2b816a4b63ec0b702239c0239e
sean stephen brian <seanstephenbrian@gmail.com>
2023-02-10 10:42:29 -0500

    set up onWelcomeScreen state condition in App
    if true, render WelcomeScreen component; if false, render Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import '../styles/app.scss';
+// dependencies:
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// components:
+import Home from './Home';
 import Welcome from "./Welcome";
 
+// styles:
+import '../styles/app.scss';
+
 function App() {
+    // state:
+    const [onWelcomeScreen, setOnWelcomeScreen] = useState(true);
+
     return (
         <div className="site-wrapper">
-            <Welcome />
+            {onWelcomeScreen? <Welcome /> : <Home />}
         </div>
     )
 }
