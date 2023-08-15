5052f39c6082fc1e1374d9f09957363bb1e460d8
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-08-15 17:27:34 -0400

    return to previous version of homepage
    will set up ghost cms later

--- app/page.js
+++ app/page.js
@@ -5,20 +5,26 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default async function Home() {
 
-    const post = await getSinglePost('first-post')
-        .then((post) => {
-            return post;
-        })
-        .catch((err) => console.log(err));
+    // const post = await getSinglePost('first-post')
+    //     .then((post) => {
+    //         return post;
+    //     })
+    //     .catch((err) => console.log(err));
     
-    if (post) {
-        return (
-            <main className={inter.className}>
-                <h1>{post.title}</h1>
-                <div dangerouslySetInnerHTML={{__html: post.html}} />
-            </main>
-        )
-    } else {
-        return;
-    }  
+    // if (post) {
+    //     return (
+    //         <main className={inter.className}>
+    //             <h1>{post.title}</h1>
+    //             <div dangerouslySetInnerHTML={{__html: post.html}} />
+    //         </main>
+    //     )
+    // } else {
+    //     return;
+    // }  
+
+    return (
+        <main className={inter.className}>
+            coming soon :)
+        </main>
+    )
 }
