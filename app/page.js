156ee916076ba625d956c909c734f4837648c898
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-08-15 15:14:46 -0400

    retrieve & render text content from ghost cms

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
+import { getSinglePost } from '@/lib/posts';
+
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
-    return (
-        <main className={inter.className}>
-            coming soon :)
-        </main>
-    )
+export default async function Home() {
+
+    const post = await getSinglePost('first-post')
+        .then((post) => {
+            return post;
+        })
+        .catch((err) => console.log(err));
+    
+    if (post) {
+        return (
+            <main className={inter.className}>
+                <h1>{post.title}</h1>
+                <div dangerouslySetInnerHTML={{__html: post.html}} />
+            </main>
+        )
+    } else {
+        return;
+    }  
 }
